6b5d308a1d86d9b8bdaa8a2bbd2382cc2cacf665
rish-16 <mail.rishabh.anand@gmail.com>
2019-07-11 22:46:20 +0800

    Updated to Dashboard

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBY8H2AtuzMXVX7IevYyn0Wr8PdiYaIrGU",
+    authDomain: "test-a1287.firebaseapp.com",
+    databaseURL: "https://test-a1287.firebaseio.com",
+    projectId: "test-a1287",
+    storageBucket: "test-a1287.appspot.com",
+    messagingSenderId: "494992214705",
+    appId: "1:494992214705:web:38a77d4c9514b154"
+};
+firebase.initializeApp(firebaseConfig);
+
